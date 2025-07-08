0414da887666cc7e76b579c7e12b0f7f16876a64
Reza <rezajafarpoor222@gmail.com>
2025-07-08 16:58:13 +0330

    Basic functionality for creating transaction

---
+++ Backend/Features/CreateTransaction/CreateTransaction.cs
@@ -0,0 +1,21 @@
+using Backend.Shared.Domain;
+using Backend.Shared.Interfaces;
+using Backend.Shared.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Features.CreateTransaction;
+
+public record CreateTransactionDto(string IncomeType, int Amount, string Description);
+
+public class CreateTransaction : IEndpoint
+{
+    public void Register(IEndpointRouteBuilder app)
+         => app.MapPost("/transaction", async ([FromBody] CreateTransactionDto dto, ApplicationContext dbContext) =>
+         {
+             // Get user Id from token then create the transaction
+             //  var user = await dbContext.Users.FirstOrDefaultAsync(u => u.Id == token);
+             //  var transaction = Transaction.CreateTransaction(dto.IncomeType, dto.Amount, dto.Description, user);
+             //  save transaction and return
+         });
+}
--- Backend/Shared/Domain/Transaction.cs
+++ Backend/Shared/Domain/Transaction.cs
@@ -1,7 +1,12 @@
+using Backend.Features.CreateAccount;
+using Backend.Features.CreateTransaction;
+
 namespace Backend.Shared.Domain;
 
 public class Transaction
 {
+    public Transaction() { }
+
     public Guid Id { get; set; }
     public string IncomeType { get; set; } = string.Empty;
     public int Amount { get; set; }
@@ -9,4 +14,17 @@ public class Transaction
     public string Description { get; set; } = string.Empty;
     public Guid UserId { get; set; }
     public User? User { get; set; }
+    private Transaction(string incomeType, int amount, string description, User user)
+    {
+        IncomeType = incomeType;
+        Amount = amount;
+        Description = description;
+        DateTime = DateTime.UtcNow;
+        User = user;
+        UserId = user.Id;
+    }
+
+    public static Transaction CreateTransaction(string incomeType, int amount, string description, User user)
+         => new(incomeType, amount, description, user);
+
 }
