f861213e4d0ee14d38490746d02a6053dc0ac1de
Reza <rezajafarpoor222@gmail.com>
2025-07-10 16:10:17 +0330

    Delete transaction implemented

--- Backend/Features/DeleteTransaction/DeleteTransaction.cs
+++ Backend/Features/DeleteTransaction/DeleteTransaction.cs
@@ -1,6 +1,8 @@
+using System.Security.Claims;
 using Backend.Shared.Interfaces;
 using Backend.Shared.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Features.DeleteTransaction;
 
@@ -9,8 +11,16 @@ namespace Backend.Features.DeleteTransaction;
 public class DeleteTransaction : IEndpoint
 {
     public void Register(IEndpointRouteBuilder app)
-        => app.MapGroup("transaction").MapDelete("/transaction", async ([FromBody] Guid id, ApplicationContext dbContext) =>
+        => app.MapGroup("transaction")
+        .MapDelete("/transaction", async ([FromBody] Guid id, HttpContext context, ApplicationContext dbContext) =>
      {
-
-     }).WithTags("Transaction");
+         var token = context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+         Guid.TryParse(token, out Guid userId);
+         var deleteResult = await dbContext.Transactions.Where(t => t.UserId == userId & t.Id == id)
+         .ExecuteDeleteAsync();
+         return deleteResult > 0
+         ? Results.NoContent()
+         : Results.NotFound();
+     }).WithTags("Transaction")
+     .RequireAuthorization();
 }
